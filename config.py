e06ffa00a84f22f38a302c49c9bd42b65da868ae
BigTianYang <799696060@qq.com>
2025-04-20 15:28:48 +0800

    获取配置

--- config.py
+++ config.py
@@ -1,1 +1,25 @@
 DEEPSEEK_API_KEY = "sk-6304c16981934da99f301329fffa2c56"
+
+#获取AINAME
+try:
+    with open("ainame.txt", "r", encoding="utf-8") as f:
+        AINAME = f.read()
+
+except FileNotFoundError:
+    print("未找到 ainame.txt 文件，请检查文件路径。")
+    exit(1)
+except Exception as e:
+    print(f"读取文件时出现错误: {e}")
+    exit(1)
+
+#获取USERNAME
+try:
+    with open("username.txt", "r", encoding="utf-8") as f:
+        USERNAME = f.read()
+
+except FileNotFoundError:
+    print("未找到 username.txt 文件，请检查文件路径。")
+    exit(1)
+except Exception as e:
+    print(f"读取文件时出现错误: {e}")
+    exit(1)
