a403b4c04a8f457cce9fa98cdbd739f8629aa825
BigTianYang <799696060@qq.com>
2025-04-20 16:19:33 +0800

    主方法

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from duihua import *
 from caidan import *
+from rduihua import *
 
 while True:
     try:
         num = int(input("1.开始对话\n2.设置\n0.退出\nTip：首次对话请先设置称呼，否则对话内将使用默认称呼\n请选择对应的功能：\n"))
         if num == 1:
-            duihua()
+            num1 = int(input("1.普通模型\n2.思考模型\n0.退出\n请选择对应的功能：\n"))
+            if num1 == 1:
+                duihua()
+            if num1 == 2:
+                rduihua()
+            if num == 0:
+                break
         if num == 2:
             config()
         if num == 0:
